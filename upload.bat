8d8524cf13498c81fa9a899fa6b676bed6187848
Saroj Poudel <hah.ptit@gmail.com>
2019-09-24 10:37:05 -0700

    CS4800:Test

--- upload.bat
+++ upload.bat
@@ -1,5 +1,5 @@
 git add --all
-git config --global user.name "Hao Huynh"
+git config --global user.name "Saroj Poudel"
 git config --global user.email hah.ptit@gmail.com
-git commit -m "CS4800:12%"
+git commit -m "CS4800:Test"
 git push origin master
